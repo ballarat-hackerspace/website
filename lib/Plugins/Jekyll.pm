9e6bc38c8ae7f838d46a9708d74825e216d123fd
firnsy <firnsy@gmail.com>
2015-07-15 10:10:28 +1000

    updated: now support text and html formats for blog posting.

--- lib/Plugins/Jekyll.pm
+++ lib/Plugins/Jekyll.pm
@@ -37,21 +37,9 @@ use constant BLOG_STYLE => 'markdown';
 
 our $VERSION = '1.1';
 
-has conf => sub { +{} };
-
-# short circuit grep
-sub sgrep(&@) {
-  my $t = shift @_;
-  for (@_) { return 1 if &$t };
-  return 0;
-}
-
-# check for element in array
-sub in_array {
-  my $n = shift;
-  my @h = ref $_[0] eq 'ARRAY' ? @{$_[0]} : @_;
-  return scalar(sgrep { $n eq $_ } @h);
-}
+has conf  => sub { +{} };
+has files => sub { +[] };
+has posts => sub { +[] };
 
 sub register {
   my ($plugin, $app, $conf) = @_;
@@ -62,34 +50,22 @@ sub register {
 
   $plugin->conf($conf) if $conf;
 
+  # prepare the cache
+  $plugin->_cache_posts($conf->{directory});
+
   $app->helper(blog_summary => sub {
     my $self = shift;
-    my @entries = ();
+    my @posts = @{$plugin->posts};
 
     my %params = @_ > 1 ? @_ : ref $_[0] eq 'HASH' ? %{ $_[0] } : ();
 
-    if (opendir DIR, $conf->{directory}) {
-      my @files = readdir(DIR);
-      closedir(DIR);
-
-      # filter only valid filenames and sort in reverse
-      @files = sort {$b cmp $a}
-                 grep { /^\d{4}-\d{2}-\d{2}-[^\.]+\.\w+/ } @files;
-
-      # parse files and filter valid parses only
-      @entries = grep { $_->{status} == 200 }
-                   map { $self->blog_parse_file($_, content => 0) } @files;
-
-    }
-
     # parse any tags
-    if ($params{tags} && @entries) {
-      @entries = grep { in_array($params{tags}, $_->{tags}) } @entries;
+    if ($params{tags} && @posts) {
+      @posts = grep { _in_array($params{tags}, $_->{tags}) } @posts;
     }
 
-    return \@entries;
+    return \@posts;
   });
-
 
   $app->helper(blog_parse_entry => sub {
     my ($self, $date, $slug) = @_;
@@ -103,72 +79,139 @@ sub register {
       $date = "20$1-$2-$3" ;
     }
 
-    my $stash = $self->blog_parse_file("$date-$slug.md");
+    my $file = $plugin->_get_file($date, $slug);
+    my $stash = $plugin->_parse_file($file);
 
     return $stash;
   });
+}
 
-  $app->helper(blog_parse_file => sub {
-    my ($self, $filename) = (shift, shift);
+sub _cache_posts {
+  my $self = shift;
+  my $path = shift;
 
-    my %params = @_ > 1 ? @_ : ref $_[0] eq 'HASH' ? %{ $_[0] } : ();
+  my @files = ();
+  my @posts = ();
 
-    # defaults
-    $params{content} //= 1;
-    $params{excerpt} //= 1;
+  if (opendir DIR, $path) {
+    @files = readdir(DIR);
+    closedir(DIR);
 
-    # build our path
-    my $path = File::Spec->catpath(undef, $conf->{directory}, $filename);
+    # filter only valid filenames and sort to reverse chronological order
+    @files = sort {$b cmp $a}
+               grep { /^\d{4}-\d{2}-\d{2}-[^\.]+\.\w+/ } @files;
 
-    # ensure the path exists
-    return { status => 404, bytes => undef } unless -r $path;
+    # parse files and filter valid parses only
+    @files = grep { $self->_parse_file($_, content => 0)->{status} == 200 } @files;
+    @posts = map { $self->_parse_file($_, content => 0) } @files;
+  }
 
-    # slurp
-    my $bytes = slurp $path;
+  $self->files(\@files);
+  $self->posts(\@posts);
+}
 
-    # ensure header exists
-    return { status => 500, bytes => undef } unless $bytes =~ m/---(.*)---\r?\n/s;
+sub _get_file {
+  my ($self, $date, $slug) = @_;
 
-    my $stash = Load($1);
-    $bytes =~ s/---.*---\r?\n//s;
+  # convert form YYYYMMDD to Jekyll form
+  if ($date =~ m/(\d{4})(\d{2})(\d{2})/) {
+    $date = "$1-$2-$3"
+  }
+  # convert form YYMMDD to Jekyll form
+  elsif ($date =~ m/(\d{2})(\d{2})(\d{2})/) {
+    $date = "20$1-$2-$3" ;
+  }
 
-    $stash->{style} = 'unknown';
+  my $prefix = "$date-$slug.";
 
-    # load filename implied details
-    my $file = basename($path);
+  my ($file) = grep { m/^$prefix/ } @{$self->files};
 
-    if ($file =~ m/^(\d{4}-\d{2}-\d{2})-([^\.]+)\.(\w+)/) {
-      $stash->{date} = Time::Piece->strptime($1, "%Y-%m-%d");
-      $stash->{slug} = $2;
-      $stash->{extension} = lc $3;
-      $stash->{style} = TYPE_MAP->{lc $3} // 'unknown';
+  return $file;
+}
+
+sub _parse_file {
+  my ($self, $filename) = (shift, shift);
+
+  my %params = @_ > 1 ? @_ : ref $_[0] eq 'HASH' ? %{ $_[0] } : ();
+
+  # defaults
+  $params{content} //= 1;
+  $params{excerpt} //= 1;
+
+  # build our path
+  my $path = File::Spec->catpath(undef, $self->conf->{directory}, $filename);
+
+  # ensure the path exists
+  return { status => 404, bytes => undef } unless -r $path;
+
+  # slurp
+  my $bytes = slurp $path;
+
+  # ensure header exists
+  return { status => 500, bytes => undef } unless $bytes =~ m/---(.*)---\r?\n/s;
+
+  my $stash = Load($1);
+  $bytes =~ s/---.*---\r?\n//s;
+
+  # initiliase some members
+  $stash->{content} = '';
+  $stash->{excerpt} = '';
+  $stash->{style} = 'unknown';
+
+  # load filename implied details
+  my $file = basename($path);
+
+  if ($file =~ m/^(\d{4}-\d{2}-\d{2})-([^\.]+)\.(\w+)/) {
+    $stash->{date} = Time::Piece->strptime($1, "%Y-%m-%d");
+    $stash->{slug} = $2;
+    $stash->{extension} = lc $3;
+    $stash->{style} = TYPE_MAP->{lc $3} // 'unknown';
+  }
+
+  # split tags
+  $stash->{tags} = [ map { trim $_ } split /,/, $stash->{tags} ] if $stash->{tags};
+
+  # grab excerpt as appropriate
+  if ($params{excerpt} != 0) {
+    $stash->{excerpt} = trim $1 if $bytes =~ m/(.*)<!--more-->\r?\n/s;
+
+    if ($stash->{style} eq 'markdown') {
+      $stash->{excerpt} = markdown($stash->{excerpt});
     }
-
-    # split tags
-    $stash->{tags} = [ map { trim $_ } split /,/, $stash->{tags} ] if $stash->{tags};
-
-    # grab excerpt as appropriate
-    if ($params{excerpt} != 0) {
-      $stash->{excerpt} = trim $1 if $bytes =~ m/(.*)<!--more-->\r?\n/s;
-
-      if ($stash->{style} eq 'markdown') {
-        $stash->{excerpt} = markdown($stash->{excerpt});
-      }
+    elsif ($stash->{style} eq 'text') {
+      $stash->{excerpt} = '<pre>'.$stash->{excerpt}.'</pre>';
     }
+  }
 
-    # store content as appropriate
-    if ($params{content} != 0) {
-      $stash->{content} = trim $bytes;
+  # store content as appropriate
+  if ($params{content} != 0) {
+    $stash->{content} = trim $bytes;
 
-      if ($stash->{style} eq 'markdown') {
-        $stash->{content} = markdown($stash->{content});
-      }
+    if ($stash->{style} eq 'markdown') {
+      $stash->{content} = markdown($stash->{content});
     }
+    elsif ($stash->{style} eq 'text') {
+      $stash->{content} = '<pre>'.$stash->{content}.'</pre>';
+    }
+  }
 
-    $stash->{status} = 200;
+  $stash->{status} = 200;
 
-    return $stash;
-  });
+  return $stash;
+}
+
+# short circuit grep
+sub _sgrep(&@) {
+  my $t = shift @_;
+  for (@_) { return 1 if &$t };
+  return 0;
+}
+
+# check for element in array
+sub _in_array {
+  my $n = shift;
+  my @h = ref $_[0] eq 'ARRAY' ? @{$_[0]} : @_;
+  return scalar(_sgrep { $n eq $_ } @h);
 }
 
 1;
