3937a36829901cd8928a06bb3dacdba5b167461d
firnsy <firnsy@ballarathackerspace.org.au>
2018-05-12 11:09:08 +1000

    updated: clean up unused JS plugins.

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,5 @@
 $(document).ready(function() {
-
-  /* ======= Twitter Bootstrap hover dropdown ======= */
-  /* Ref: https://github.com/CWSpear/bootstrap-hover-dropdown */
-  /* apply dropdownHover to all elements with the data-hover="dropdown" attribute */
-
-  $('[data-hover="dropdown"]').dropdownHover();
-
-  /* ======= jQuery Responsive equal heights plugin ======= */
-  /* Ref: https://github.com/liabru/jquery-match-height */
-
-  $('#who .item-inner').matchHeight();
-  $('#testimonials .item-inner .quote').matchHeight();
-  $('#latest-blog .item-inner').matchHeight();
-  $('#services .item-inner').matchHeight();
-  $('#team .item-inner').matchHeight();
-
-  /* ======= jQuery Placeholder ======= */
-  /* Ref: https://github.com/mathiasbynens/jquery-placeholder */
-
-  $('input, textarea').placeholder();
-
-  /* ======= jQuery FitVids - Responsive Video ======= */
-  /* Ref: https://github.com/davatron5000/FitVids.js/blob/master/README.md */
-  $(".video-container").fitVids();
-
-
-  /* ======= Fixed Header animation ======= */
-
+  // initialise header animation
   $(window).on('scroll', function() {
     if ($(window).scrollTop() > 80) {
       $('#header').addClass('header-shrink');
@@ -35,4 +8,7 @@ $(document).ready(function() {
       $('#header').removeClass('header-shrink');
     }
   });
+
+  // initialise tooltips
+  $('[data-toggle="tooltip"]').tooltip();
 });
